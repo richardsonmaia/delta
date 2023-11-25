bfc2dc3bbe529065cfc2deece0e79a683d5362ff
richardson <jr_maia@outlook.com>
2023-11-25 15:15:46 -0300

    finalização do TabBar

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"; 
+import { Ionicons } from '@expo/vector-icons';
 
 import carrinhos from './pages/carrinhos.js'
 import categoria from './pages/categoria.js'
@@ -9,6 +10,7 @@ import home from './pages/home.js'
 const Tab = createBottomTabNavigator();
 
 const screenOptions = {
+    tabBarActiveTintColor: '#6C008B',
     tabBarShowLabel: false,
     tabBarHiderOnKeyboard:true,
     headerShow: false,
@@ -26,23 +28,59 @@ const Routes = () => {
     return(
         <Tab.Navigator screenOptions={screenOptions}>
             <Tab.Screen 
-            name="home"
+            name="Home"
             component={home}
+            options={{
+                tabBarIcon: ({ color, size, focused}) => {
+                    if(focused){
+                        return<Ionicons name="home" size={size} color={color} />
+                    }
+
+                    return <Ionicons name="home-outline" size={size} color={color}/>
+                } 
+            }}
             />
 
         <Tab.Screen 
-            name="categoria"
+            name="Categoria"
             component={categoria}
+            options={{
+                tabBarIcon: ({ color, size, focused}) => {
+                    if(focused){
+                        return<Ionicons name="bookmark" size={size} color={color} />
+                    }
+
+                    return <Ionicons name="bookmark-outline" size={size} color={color}/>
+                }
+            }}
             />
 
         <Tab.Screen 
-            name="favoritos"
+            name="Favoritos"
             component={favoritos}
+            options={{
+                tabBarIcon: ({ color, size, focused}) => {
+                    if(focused){
+                        return<Ionicons name="heart" size={size} color={color} />
+                    }
+
+                    return <Ionicons name="heart-outline" size={size} color={color}/>
+                }
+            }}
             /> 
 
         <Tab.Screen 
-            name="carrinhos"
+            name="Carrinhos"
             component={carrinhos}
+            options={{
+                tabBarIcon: ({ color, size, focused}) => {
+                    if(focused){
+                        return<Ionicons name="cart" size={size} color={color} />
+                    }
+
+                    return <Ionicons name="cart" size={size} color={color}/>
+                }
+            }}
             />
         </Tab.Navigator> 
     );
